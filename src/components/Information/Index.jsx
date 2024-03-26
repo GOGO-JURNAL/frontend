aa1fd0970500e0cd0cb5799487204c898f7840f3
khoican <ibnukhoirulprasetyo@gmail.com>
2024-03-27 01:12:09 +0700

    feat: set information for dinamis data

--- src/components/Information/Index.jsx
+++ src/components/Information/Index.jsx
@@ -1,7 +1,24 @@
 import { FileEarmarkTextFill, PeopleFill } from 'react-bootstrap-icons'
 import InformationMenu from './Menu'
+import { useEffect, useState } from 'react'
+import { getLecturers, getJournal } from '../../../services/journal'
 
 const Information = () => {
+    const [lecturers, setLecturers] = useState(0)
+    const [articles, setArticles] = useState(0)
+
+    useEffect(() => {
+        getLecturers().then((data) => setLecturers(data.length))
+    })
+
+    useEffect(() => {
+        getJournal().then((data) => setArticles(data))
+    })
+    const total =
+        Number(articles.scopus) +
+        Number(articles.penelitian) +
+        Number(articles.pengabdian)
+
     const iconStyle = 'visible text-blue fs-1'
 
     return (
@@ -11,12 +28,12 @@ const Information = () => {
             <InformationMenu
                 icon={<PeopleFill className={iconStyle} />}
                 text="Lecturers"
-                value={10}
+                value={lecturers}
             />
             <InformationMenu
                 icon={<FileEarmarkTextFill className={iconStyle} />}
                 text="Articles"
-                value={100}
+                value={total}
             />
         </section>
     )
